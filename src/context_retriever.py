52b6504b39c558be373a8db0e4f2eada70b5c232
Dheeraj Chillamcharla <chillamcharladheeraj@gmail.com>
2024-11-01 17:27:36 -0400

    Bug: Database path has been updated

--- src/context_retriever.py
+++ src/context_retriever.py
@@ -1,10 +1,12 @@
-from load_vector_db import embeddings, vector_store
+from load_vector_db import initialize_vector_db
+import os
 
 
 def context_retriever(query):
     # Retrieve and generate using the relevant snippets of the blog.
+    db_path = os.path.join(os.getcwd(), "../chroma_langchain_db")
+    vector_store = initialize_vector_db(db_path)
     retriever = vector_store.as_retriever(search_kwargs={"k": 3})
-
     docs = retriever.invoke(query)
     context = "\n\n".join([doc.page_content for doc in docs])
 
