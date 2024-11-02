bd69686e6df02703e419e1c19e426b60a86b87b6
Dheeraj Chillamcharla <chillamcharladheeraj@gmail.com>
2024-11-02 14:30:56 -0400

    Enhancement: Gradio UI

--- src/gradio_ui.py
+++ src/gradio_ui.py
@@ -5,6 +5,13 @@ demo = gr.ChatInterface(
     rag,
     type="messages",
     title="NOV",
+    chatbot=gr.Chatbot(
+        type="messages",
+        placeholder="<strong>Hello there!</strong><br>I'm a product specialist at NOV. Would love to answer any questions you have about our products!",
+        height=600,
+    ),
+    theme="soft",
+    textbox=gr.Textbox(placeholder="Ask me a question", container=True),
 )
 
 if __name__ == "__main__":
