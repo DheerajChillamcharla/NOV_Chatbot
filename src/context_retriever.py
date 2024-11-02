c01ea685dc6485923aa06ef392339e5d4d55a1f9
Dheeraj Chillamcharla <chillamcharladheeraj@gmail.com>
2024-11-02 15:26:00 -0400

    Enhancement: Retriving strategy updated

--- src/context_retriever.py
+++ src/context_retriever.py
@@ -6,7 +6,15 @@ def context_retriever(query):
     # Retrieve and generate using the relevant snippets of the blog.
     db_path = os.path.join(os.getcwd(), "../chroma_langchain_db")
     vector_store = initialize_vector_db(db_path)
-    retriever = vector_store.as_retriever(search_kwargs={"k": 3})
+    retriever = vector_store.as_retriever(
+        search_type="mmr",
+        search_kwargs={
+            "k": 3,
+            "fetch_k": 6,
+            "lambda_mult": 0.75,
+            "score_threshold": 0.75,
+        },
+    )
     docs = retriever.invoke(query)
     context = "\n\n".join([doc.page_content for doc in docs])
     url = [doc.metadata.get("url", "No URL found") for doc in docs]
