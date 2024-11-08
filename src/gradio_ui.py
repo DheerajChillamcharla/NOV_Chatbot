cb33121586ee6f9d5bc1ad0e36b8019e9d0fddaa
Dheeraj Chillamcharla <chillamcharladheeraj@gmail.com>
2024-11-07 22:28:18 -0500

    Enhancement: Examples and Description added

--- src/gradio_ui.py
+++ src/gradio_ui.py
@@ -10,8 +10,13 @@ demo = gr.ChatInterface(
         placeholder="<strong>Hello there!</strong><br>I'm a product specialist at NOV. Would love to answer any questions you have about our products!",
         height=600,
     ),
+    description='Data is sourced from NOV’s publicly available document library and website. All rights and '
+                'acknowledgments belong to NOV.',
     theme="soft",
     textbox=gr.Textbox(placeholder="Ask me a question", container=True),
+    examples=['Can you tell me the specs for Triplex Plunger Pump',
+              'Tell me about recommended best practices for Coiled Tubing Purging',
+              'Can you tell me about fishing jar systems?'],
 )
 
 if __name__ == "__main__":
